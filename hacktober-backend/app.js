9d4942a23fdfd7395020997ece8325e806659e79
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-10-18 00:29:53 -0400

    created app.js for backend

---
+++ hacktober-backend/app.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+  
+const app = express();
+const PORT = 3000;
+  
+app.listen(PORT, (error) =>{
+    if (!error)
+        console.log("listening on port " + PORT);
+    else 
+        console.log("Error creating server: ", error);
+    }
+);
